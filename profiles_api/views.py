a52902a6cec4d424edbfbf83faa7e1ee0b748a2f
Sujit Packiaraj <sujit.pack@gmail.com>
2021-10-28 01:20:25 -0700

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -106,3 +108,17 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+    # def list(self, request):
+    #     print(request)
+    #     print(dir(request))
+    #     print(request._request)
+    #     print(dir(request._request))
+    #     print(request._request.get_full_path())
+    #
+    #     return Response({'message': 'Hello, World!'})
+    #
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
